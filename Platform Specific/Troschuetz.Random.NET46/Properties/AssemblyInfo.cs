4ce134650dc1d38c67268e52350d6784f1f74290
Alessio Parma <alessio.parma@gmail.com>
2015-10-10 13:54:03 +0200

    using shared projects

--- Platform Specific/Troschuetz.Random.NET46/Properties/AssemblyInfo.cs
+++ Platform Specific/Troschuetz.Random.NET46/Properties/AssemblyInfo.cs
@@ -1,36 +1,45 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
+﻿/*
+ * Copyright © 2006 Stefan Troschütz (stefan@troschuetz.de)
+ * Copyright © 2012-2016 Alessio Parma (alessio.parma@gmail.com)
+ *
+ * This file is part of Troschuetz.Random Class Library.
+ *
+ * Troschuetz.Random is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ *
+ * See the GNU Lesser General Public License for more details.
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("Troschuetz.Random.NET46")]
-[assembly: AssemblyDescription("")]
+using System.Reflection;
+using System.Security;
+
+// General Information about an assembly is controlled through the following set of attributes.
+// Change these attribute values to modify the information associated with an assembly.
+[assembly: AssemblyTitle("Troschuetz.Random")]
+[assembly: AssemblyDescription("Fully managed library providing various random number generators and distributions.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Troschuetz.Random.NET46")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyProduct("Troschuetz.Random")]
+[assembly: AssemblyCopyright("Copyright © Stefan Troschütz")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
-// COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
-
-// The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("6a27fa7a-4e08-43d5-ac49-5a460ade00e6")]
-
 // Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+// 
+// Major Version Minor Version Build Number Revision
+// 
+// You can specify all the values or you can default the Build and Revision Numbers by using the '*'
+// as shown below: [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("4.0.1")]
+[assembly: AssemblyFileVersion("4.0.1")]
+
+// Specifies that an assembly cannot cause an elevation of privilege.
+[assembly: SecurityTransparent]
