3bd9ffa9e40e2f6dc33f350f0de33725d0b8b501
Alessio Parma <alessio.parma@gmail.com>
2016-10-17 10:57:54 +0200

    v4.0.6

--- Platform Specific/Troschuetz.Random.NETSTD11/Properties/AssemblyInfo.cs
+++ Platform Specific/Troschuetz.Random.NETSTD11/Properties/AssemblyInfo.cs
@@ -39,7 +39,7 @@ using System.Security;
 // You can specify all the values or you can default the Build and Revision Numbers by using the '*'
 // as shown below: [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("4.0.3")]
-[assembly: AssemblyFileVersion("4.0.5")]
+[assembly: AssemblyFileVersion("4.0.6")]
 
 // Specifies that an assembly cannot cause an elevation of privilege.
 [assembly: SecurityTransparent]
