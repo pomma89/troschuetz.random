46b36926350d0a7a8152434683fdcd66ede2f65b
Alessio Parma <alessio.parma@gmail.com>
2017-01-29 11:19:26 +0100

    ...

---
+++ core-experiments/test/Troschuetz.Random.UnitTests/Program.cs
@@ -0,0 +1,17 @@
+﻿using NUnitLite;
+using System.Reflection;
+
+namespace ClassLibrary
+{
+    internal static class Program
+    {
+        public static int Main(string[] args)
+        {
+#if NET40
+            return new AutoRun(typeof(Program).Assembly).Execute(args);
+#else
+            return new AutoRun(typeof(Program).GetTypeInfo().Assembly).Execute(args);
+#endif
+        }
+    }
+}
