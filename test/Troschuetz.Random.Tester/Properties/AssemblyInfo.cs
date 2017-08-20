340a53913ca7967010300f957ff18f4a5b2aec7a
Alessio Parma <alessio.parma@gmail.com>
2017-08-20 18:05:36 +0200

    v4.2.0

--- test/Troschuetz.Random.Tester/Properties/AssemblyInfo.cs
+++ test/Troschuetz.Random.Tester/Properties/AssemblyInfo.cs
@@ -48,8 +48,8 @@ using System.Security;
 //
 // You can specify all the values or you can default the Build and Revision Numbers by using the '*'
 // as shown below: [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("4.1.3")]
-[assembly: AssemblyFileVersion("4.1.3")]
+[assembly: AssemblyVersion("4.2.0")]
+[assembly: AssemblyFileVersion("4.2.0")]
 
 // Specifies that an assembly cannot cause an elevation of privilege.
 [assembly: SecurityTransparent]
