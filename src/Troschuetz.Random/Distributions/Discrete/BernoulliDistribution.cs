84e912c7fa7818776f4e7fceea9d4076d880e6a0
Alessio Parma <alessio.parma@gmail.com>
2018-03-11 09:29:30 +0100

    fix broken checks

--- src/Troschuetz.Random/Distributions/Discrete/BernoulliDistribution.cs
+++ src/Troschuetz.Random/Distributions/Discrete/BernoulliDistribution.cs
@@ -70,7 +70,7 @@ namespace Troschuetz.Random.Distributions.Discrete
             get { return _alpha; }
             set
             {
-                if (IsValidAlpha(value)) throw new ArgumentOutOfRangeException(nameof(Alpha), ErrorMessages.InvalidParams);
+                if (!IsValidAlpha(value)) throw new ArgumentOutOfRangeException(nameof(Alpha), ErrorMessages.InvalidParams);
                 _alpha = value;
             }
         }
@@ -166,7 +166,7 @@ namespace Troschuetz.Random.Distributions.Discrete
         public BernoulliDistribution(IGenerator generator, double alpha) : base(generator)
         {
             var vp = IsValidParam;
-            if (vp(alpha)) throw new ArgumentOutOfRangeException(nameof(alpha), ErrorMessages.InvalidParams);
+            if (!vp(alpha)) throw new ArgumentOutOfRangeException(nameof(alpha), ErrorMessages.InvalidParams);
             _alpha = alpha;
         }
 
